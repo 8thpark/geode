4a2300691fcbf6b795598e185d21985bcc3bab80
kwame-Owusu <alfredowusu595@gmail.com>
2026-07-15 19:30:48 +0100

    chore: biome format

--- src/storage.ts
+++ src/storage.ts
@@ -1,8 +1,8 @@
 import { AwsClient } from "aws4fetch";
 import { endpointFor, type GeodeSettings, regionFor } from "./settings.ts";
+import { encodeKey } from "./utils/storage/encode.ts";
 import { messageFor, statusForHttp } from "./utils/storage/errors.ts";
 import { parseListObjectsXml } from "./utils/storage/xml.ts";
-import { encodeKey } from "./utils/storage/encode.ts"
 
 // ResultStatus classifies the outcome of a storage operation so callers can distinguish absent
 // objects from transient failures without parsing the message string.
@@ -63,7 +63,6 @@ export type StorageClient = {
   deleteObject: (key: string) => Promise<DeleteResult>;
   listObjects: (prefix?: string) => Promise<ListResult>;
 };
-
 
 // missingFieldFor returns the name of the first field testConnection needs but doesn't have, or
 // "" if everything required is present.
